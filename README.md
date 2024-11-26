81d5b2653aa42d9b38ba73a61f4deb34f0808a94
galdoben <galdobenedetta@gmail.com>
2024-11-26 17:20:51 +0100

    fix: function-script

--- README.md
+++ README.md
@@ -3,3 +3,10 @@ Da lì parte un timer di 10 secondi.
 Dopo 10 secondi i numeri scompaiono 
 lee appaiono invece 5 input in cui l’utente deve inserire i numeri che ha visto precedentemente, nell’ordine che preferisce.
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+quando si ferma il tempo:
+timer fermo alla 0
+spariscono i container
+escono gli input
+esce un bottone submit => al click - confronto dell 'array con i numeri inseriti negli input
+e coseguente message
--- scipt.js
+++ scipt.js
@@ -1,15 +1,15 @@
 
-
+// 5 numeri casuali
 let number1 = [Math.floor((Math.random() * 100) + 1)];
 let number2 = [Math.floor((Math.random() * 100) + 1)];
 let number3 = [Math.floor((Math.random() * 100) + 1)];
 let number4 = [Math.floor((Math.random() * 100) + 1)];
 let number5 = [Math.floor((Math.random() * 100) + 1)];
 
-
+// creazione array
 let array = [number1, number2, number3, number4, number5];
 console.log(array)
-
+//assegno elementi al DOM
 const display = document.getElementById('display');
 const container1 = document.getElementById('first-num');
 const container2 = document.getElementById('second-num');
@@ -22,12 +22,27 @@ const input3 = document.getElementById('third-inp');
 const input4 = document.getElementById('fourth-inp');
 const input5 = document.getElementById('fifth-inp');
 
-
+//visualizzo i nr random dalla pagina html su page
 container1.innerHTML = number1
 container2.innerHTML = number2
 container3.innerHTML = number3
 container4.innerHTML = number4
 container5.innerHTML = number5
-
+//creo un timer di 10sec
 let seconds = 10;
-seconds.innerHTML = seconds--;
+display.innerHTML = seconds--;
+const timer = setInterval(function(){
+    display.innerHTML = seconds;
+    if (seconds === 0){
+     clearInterval(timer)
+     
+    }
+    else{
+        seconds--
+    }
+},1000)
+
+
+
+
+
